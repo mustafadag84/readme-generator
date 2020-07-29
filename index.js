a7f7d39788ad30c0554ed650fa723b7bcfed075b
Mustafa Dag <mustafaa_dag@hotmail.com>
2020-07-28 22:00:22 -0400

    created  prompt for questions

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 const inquirer = require("inquirer");
+const fs = require('fs');
+
+inquirer.prompt([
+    {
+        type: 'input',
+        message: "what is your github username?",
+        name: 'username'
+        validate: value => {
+            if(user.length < 10){
+                return "Username is too short.";
+            }
+            else if(user.toLowerCase() != user){
+                return "Username should be lowercase.";
+            }
+            else{
+                // all validation checks passed
+                return true;
+            }
+        }
+    }
+]).then( response => {
+    console.log(response);
+});
